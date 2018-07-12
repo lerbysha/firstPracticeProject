343649bc8775d13e3eb860079c3b03204532b6f8
itisioslab <itisioslab@Mac-mini-administrator-2.local>
2018-07-12 15:59:24 +0300

    first commit

---
+++ SelfCreator/DataBaseManager.swift
@@ -0,0 +1,9 @@
+//
+//  DataBaseManager.swift
+//  SelfCreator
+//
+//  Created by itisioslab on 12.07.2018.
+//  Copyright © 2018 FirstGroupCompany. All rights reserved.
+//
+
+import Foundation
---
+++ SelfCreator/Task.swift
@@ -0,0 +1,9 @@
+//
+//  Task.swift
+//  SelfCreator
+//
+//  Created by itisioslab on 12.07.2018.
+//  Copyright © 2018 FirstGroupCompany. All rights reserved.
+//
+
+import Foundation
